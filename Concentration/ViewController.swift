f5e815e4ddc12f74f32175cd27958f3ea6229c55
ahyeonlog <ahyeonway@gmail.com>
2020-04-02 19:55:50 +0900

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ahyeon Gil on 2020/04/02.
+//  Copyright © 2020 Ahyeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
